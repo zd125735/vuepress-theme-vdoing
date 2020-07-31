cfc343ae228d5b8116c6aa630c81bdada1143e80
zhoudong <zhoudong507@126.com>
2020-07-31 15:54:54 +0800

    add coding

--- deploy.sh
+++ deploy.sh
@@ -28,9 +28,9 @@ git push -f $githubUrl master # 推送到github
 # deploy to coding
 echo 'javazd.cn' > CNAME  # 自定义域名
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:xgy/xgy.git
+  codingUrl=git@e.coding.net:wangoon/blog/blog.git
 else
-  codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+  codingUrl=https://SwyytuepLW:${CODING_TOKEN}@e.coding.net:wangoon/blog/blog.git
 fi
 git add -A
 git commit -m "${msg}"
